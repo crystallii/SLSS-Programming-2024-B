6c20ab7cdbc4808443b9e350697ec934bff508f9
Crystal Li <crystalli2606@gmail.com>
2024-04-03 12:02:09 -0700

    filled out notes on function some more

--- notes-2-2-morefunctions.py
+++ notes-2-2-morefunctions.py
@@ -1,4 +1,40 @@
 # More Functions
-# Ubial
+# Author: Crystal 
 # 3 April 2024
 
+# Implement stars function
+
+def stars(num):
+    """Returns specified number of *s"""
+    value = ""  # placeholder for return value 
+
+    if num == 0 or num == 1:
+        value = "*"
+
+    elif num > 1:
+        value = "*" * num
+
+    else:
+        value = "Sorry, can't take negative numbers."
+
+    # return "*" * num 
+    return value 
+
+
+
+# Multiply Strings
+greeting = "hello" 
+print(greeting * 5)
+
+print("the quick brown fox jumps over the lazy dog" * 2)
+
+
+# print(stars(10))    # "*****"
+# print(stars(1000))  # "********"
+
+
+# testing to see if code works
+print(stars(0))
+print(stars(1))
+print(stars(1000))
+print(stars(-1))
